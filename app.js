d3f7b7d136aea2ffaa5c09b90d3f00764f30657c
Mihai <mihaiveronica@ymail.com>
2017-10-03 15:39:17 +0300

    added game logic begining

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 var CHOICE_X = "X",
     CHOICE_O = "O",
-    choice;
+    choice,
+    gameMatrix = [],
+    PLAYER = 1,
+    COMPUTER = -1,
+    WIN_PLAYER = "You won !",
+    WIN_COMPUTER = "Computer won !",
+    DRAW = "Nobody won !";
+
+//init game matrix
+for (var i = 0; i < 3; i++) {
+    gameMatrix[i] = [];
+    for (var j = 0; j < 3; j++) {
+        gameMatrix[i][j] = 0;
+    }
+}
--- eventListeners.js
+++ eventListeners.js
@@ -1,6 +1,23 @@
 $("#X").click(function() {
     choice = CHOICE_X;
+    showGame();
 });
 $("#O").click(function() {
     choice = CHOICE_O;
+    showGame();
+});
+
+$(".cell").click(function(e) {
+    var text = $(this).text();
+    var index = $(this).attr('index');
+
+    if (!text) {
+        $(this).text(choice);
+        makeChoice(index, PLAYER);
+        makeChoiceComputer();
+        var win = checkWin();
+        if (win) {
+            alert('win ' + win);
+        }
+    }
 });
